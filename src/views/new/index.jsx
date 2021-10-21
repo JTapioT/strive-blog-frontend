5bb71a75c33a7fa7ca37fba0e53c25c0aacfb581
JTapioT <88656150+JTapioT@users.noreply.github.com>
2021-10-21 22:55:46 +0200

    Update file
    new blog post now has a file upload element.

--- src/views/new/index.jsx
+++ src/views/new/index.jsx
@@ -6,8 +6,31 @@ import "./styles.css";
 export default class NewBlogPost extends Component {
   constructor(props) {
     super(props);
-    this.state = { text: "" };
+    this.state = { text: "", category: "", title: "" };
     this.handleChange = this.handleChange.bind(this);
+    this.formData = new FormData();
+  }
+
+  /* 
+  formData.append("coverPhoto", file);
+  
+  */
+
+  async submitBlogPost(e) {
+    e.preventDefault();
+    console.log(this.state);
+
+    /* let response = await fetch("http://localhost:3001/blogPosts", {
+      method: "POST",
+      body: JSON.stringify(this.state),
+      headers: {
+        'Content-Type': "application/json"
+      }
+    }
+    ); */
+
+    //
+
   }
 
   handleChange(value) {
@@ -17,14 +40,33 @@ export default class NewBlogPost extends Component {
   render() {
     return (
       <Container className="new-blog-container">
-        <Form className="mt-5">
+        <Form
+          className="mt-5"
+          onSubmit={(e) => {
+            this.submitBlogPost(e);
+          }}
+        >
           <Form.Group controlId="blog-form" className="mt-3">
             <Form.Label>Title</Form.Label>
-            <Form.Control size="lg" placeholder="Title" />
+            <Form.Control
+              size="lg"
+              placeholder="Title"
+              value={this.state.title}
+              onChange={(e) =>
+                this.setState({ ...this.state, title: e.target.value })
+              }
+            />
           </Form.Group>
           <Form.Group controlId="blog-category" className="mt-3">
             <Form.Label>Category</Form.Label>
-            <Form.Control size="lg" as="select">
+            <Form.Control
+              size="lg"
+              as="select"
+              value={this.state.category}
+              onChange={(e) =>
+                this.setState({ ...this.state, category: e.target.value })
+              }
+            >
               <option>Category1</option>
               <option>Category2</option>
               <option>Category3</option>
@@ -40,7 +82,18 @@ export default class NewBlogPost extends Component {
               className="new-blog-content"
             />
           </Form.Group>
-          <Form.Group className="d-flex mt-3 justify-content-end">
+          <Form.Group className="d-flex mt-3 justify-content-end align-items-baseline">
+            <Form.Group className="d-flex align-items-baseline">
+            <Form.Label className="mx-2">Upload an image for blog post</Form.Label>
+            <Form.File
+              type="file"
+              id="blogCoverSubmit"
+              onChange={(event) => {
+                console.log(event.target.files[0])
+                this.formData.append("coverPhoto", event.target.files[0]);
+              }}
+            />
+            </Form.Group>
             <Button type="reset" size="lg" variant="outline-dark">
               Reset
             </Button>
