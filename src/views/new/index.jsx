6f8b3670810558763074b3b921b911d991859718
JTapioT <88656150+JTapioT@users.noreply.github.com>
2021-10-26 15:31:19 +0200

    Update index.jsx
    Url correction

--- src/views/new/index.jsx
+++ src/views/new/index.jsx
@@ -32,7 +32,7 @@ export default class NewBlogPost extends Component {
         blogId = (await response.json())._id;
         console.log(blogId);
         
-      let imgUploadResponse = await fetch("http://localhost:3001/upload",{
+      let imgUploadResponse = await fetch(`${process.env.REACT_APP_BE_PROD_URL}/upload`,{
           method: "POST",
           body: this.formData
         })
