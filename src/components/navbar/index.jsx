28637e79855fe3caead033494849bb2dc91e239b
JTapioT <88656150+JTapioT@users.noreply.github.com>
2021-10-28 15:24:23 +0200

    Update index.jsx
    Fix to button alignment

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -12,51 +12,51 @@ export default class NavBar extends Component {
             <img className="blog-navbar-brand" alt="logo" src={logo} />
           </Navbar.Brand>
           <div className="d-flex">
-          <Button
-            className="mr-2"
-            href={`${process.env.REACT_APP_BE_PROD_URL}/authors/authorsCSV`}
-            variant="outline-success"
-            size="lg"
-          >
-            All authors
-          </Button>
-          <Button
-            className="mr-2"
-            as={Link}
-            to="/register"
-            variant="outline-success"
-            size="lg"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-plus-lg"
-              viewBox="0 0 16 16"
+            <Button
+              style={{ marginRight: "1em" }}
+              href={`${process.env.REACT_APP_BE_PROD_URL}/authors/authorsCSV`}
+              variant="outline-success"
+              size="lg"
             >
-              <path d="M8 0a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2H9v6a1 1 0 1 1-2 0V9H1a1 1 0 0 1 0-2h6V1a1 1 0 0 1 1-1z" />
-            </svg>
-            Register
-          </Button>
-          <Button
-            as={Link}
-            to="/new"
-            className="blog-navbar-add-button bg-dark"
-            size="lg"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-plus-lg"
-              viewBox="0 0 16 16"
+              All authors
+            </Button>
+            <Button
+              style={{ marginRight: "1em" }}
+              as={Link}
+              to="/register"
+              variant="outline-success"
+              size="lg"
             >
-              <path d="M8 0a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2H9v6a1 1 0 1 1-2 0V9H1a1 1 0 0 1 0-2h6V1a1 1 0 0 1 1-1z" />
-            </svg>
-            Post Article
-          </Button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-plus-lg"
+                viewBox="0 0 16 16"
+              >
+                <path d="M8 0a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2H9v6a1 1 0 1 1-2 0V9H1a1 1 0 0 1 0-2h6V1a1 1 0 0 1 1-1z" />
+              </svg>
+              Register
+            </Button>
+            <Button
+              as={Link}
+              to="/new"
+              className="blog-navbar-add-button bg-dark"
+              size="lg"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-plus-lg"
+                viewBox="0 0 16 16"
+              >
+                <path d="M8 0a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2H9v6a1 1 0 1 1-2 0V9H1a1 1 0 0 1 0-2h6V1a1 1 0 0 1 1-1z" />
+              </svg>
+              Post Article
+            </Button>
           </div>
         </Container>
       </Navbar>
